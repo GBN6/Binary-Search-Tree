4dce385e851b57fdfb1cc6c5d581156856affe48
GBN6 <czesiox8@gmail.com>
2022-08-16 14:07:43 +0200

    Update driver

--- driver.js
+++ driver.js
@@ -5,3 +5,20 @@ const randomNumbersArray = (size) => {
 };
 
 const tree = new Tree(randomNumbersArray(20));
+console.log('Balanced:', tree.balanced());
+console.log('Lever Order =>', tree.levelOrder());
+console.log('Preorder =>', tree.preorder());
+console.log('Inorder =>', tree.inorder());
+console.log('Postorder =>', tree.postorder());
+
+for (let i = 0; i < 5; i++) {
+    tree.insert(Math.floor(Math.random() * 1000));
+  }
+console.log('Balanced:', tree.balanced());
+
+tree.reBalance();
+console.log('Balanced:', tree.balanced());
+console.log('Lever Order =>', tree.levelOrder());
+console.log('Preorder =>', tree.preorder());
+console.log('In-order =>', tree.inorder());
+console.log('Post-order =>', tree.postorder());
